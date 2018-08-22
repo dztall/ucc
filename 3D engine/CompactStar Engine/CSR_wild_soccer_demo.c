b7b9e655e1a1490409efacfa25e36b920171a708
Jean-Milost Reymond <jean_milost@hotmail.com>
2018-08-22 16:35:44 -0300

    - cleaned: comments

--- 3D engine/CompactStar Engine/CSR_wild_soccer_demo.c
+++ 3D engine/CompactStar Engine/CSR_wild_soccer_demo.c
@@ -1,8 +1,8 @@
 ﻿/*****************************************************************************
  * ==> Wild soccer game demo ------------------------------------------------*
  *****************************************************************************
- * Description : A cross football game demo. Swipe up or down to walk, and   *
- *               left or right to rotate. Tap to shoot the ball              *
+ * Description : A wild soccer game demo. Swipe up or down to walk, and left *
+ *               or right to rotate. Tap to shoot the ball                   *
  * Developer   : Jean-Milost Reymond                                         *
  * Copyright   : 2015 - 2018, this file is part of the Minimal API. You are  *
  *               free to copy or redistribute this file, modify it, or use   *
